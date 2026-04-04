d2858058189dd195cc17e9ec7cb60418151fa6eb
volonte malisava <volontemuhi@gmail.com>
2026-04-04 21:11:41 +0200

    modification du router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import { useEffect, useState } from "react"
-import Chat from "./components/chat/Chat"
-import Detail from "./components/detail/Detail"
-import List from "./components/list/List"
-import Login from "./components/login/Login"
-import Notification from "./components/notification/Notification"
+import { useEffect } from "react"
+import AppRoutes from "./routes/Routes"
 import { onAuthStateChanged } from "firebase/auth"
 import { auth } from "./lib/firebase"
 import { useUserStore } from "./lib/userStore"
-import { useChatStore } from "./lib/useChatStore"
 
 const App = () => {
-  //const [user,setUser]=useState(false)
-
   const {currentUser,isLoading,fetchUserInfo}=useUserStore()
-  const {chatId}=useChatStore()
 
   useEffect(()=>{
     const unSub=onAuthStateChanged(auth,(user)=>{
@@ -24,19 +16,10 @@ const App = () => {
     }
   },[fetchUserInfo])
   
-  if(isLoading)return <div className="loading">Chargement...</div>
+  if(isLoading) return <div className="loading">Chargement...</div>
+
   return (
-    <div className='container'>
-      {
-        currentUser?(<>
-          <List/>
-          {chatId &&<Chat/>}
-          {chatId && <Detail/>}
-          </>):
-          (<Login/>)
-      }
-      <Notification/>
-    </div>
+    <AppRoutes/>
   )
 }
 
